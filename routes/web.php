56ef5a8f93403418086efc813eb236afbdda7a2a
simondriesen <simon.driesen@hotmail.com>
2016-09-28 10:38:37 +0200

    Added migration/controller example and database setup.

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/students', 'StudentsController@index');
+Route::get('/students/create', 'StudentsController@create');
