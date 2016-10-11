b759a4152dff881347f0b0190175ddd5e41e3a6b
Max Vranken <maxvranken@Maxs-MacBook-Pro.local>
2016-10-11 14:45:10 +0200

    Added logs focus design

--- resources/views/focus/logs.blade.php
+++ resources/views/focus/logs.blade.php
@@ -1,5 +1,27 @@
 @extends('layouts/hisfa')
 
 @section('content')
-    <h1>Logs</h1>
+
+    <div class="log_focus">
+        <div class="log_title">
+            <div class="title_dot" style="width: 10px; height: 10px; background-color: #745FA4;"></div>
+            <p>Latest Logs</p>
+        </div>
+        <div class="log_log_focus" id="log1_focus">
+            <span class="log_timestamp_focus">01/01/16 11:16</span>
+            <span class="log_text_focus">P15 (4m) +1st</span>
+        </div>
+        <div class="log_log_focus" id="log2_focus">
+            <span class="log_timestamp_focus">01/01/16 11:16</span>
+            <span class="log_text_focus">P15 (4m) +1st</span>
+        </div>
+        <div class="log_log_focus" id="log3_focus">
+            <span class="log_timestamp_focus">01/01/16 11:16</span>
+            <span class="log_text_focus">P15 (4m) +1st</span>
+        </div>
+        <div class="log_log_focus" id="log4_focus">
+            <span class="log_timestamp_focus">01/01/16 11:16</span>
+            <span class="log_text_focus">P15 (4m) +1st</span>
+        </div>
+    </div>
 @endsection
