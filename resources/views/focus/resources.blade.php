99b987a926bc6304c31bd3bcb24e0b4d78a8d4aa
thomasvanmalderen <thomas.vanmalderen@student.thomasmore.be>
2016-10-11 18:26:44 +0000

    display all resources on resource page

--- resources/views/focus/resources.blade.php
+++ resources/views/focus/resources.blade.php
@@ -1,5 +1,26 @@
 @extends('layouts/hisfa')
 
 @section('content')
-    <h1>Resources</h1>
+    <div class="resource">
+        <div class="resource_title">
+            <div class="title_dot" style="width: 10px; height: 10px; background-color: #51B8F2;"></div>
+            <p>Resources</p>
+        </div>
+        <div class="resource_container">
+            @foreach($resources as $resource)
+                <div class="resourcetype" id="resource {{ $resource->id }}">
+                    <div class="rsrcetype">{{ $resource->name }}</div>
+                </div>
+            @endforeach
+
+        </div>
+        <div class="resource_stock_container">
+            @foreach($resources as $resource)
+                <div class="stock">
+                    <div class="resource_number_stock">{{$resource->quantity}}<span class="ton">ton</span></div>
+                </div>
+            @endforeach
+
+        </div>
+    </div>
 @endsection
