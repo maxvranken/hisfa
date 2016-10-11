f1120b4c394ced965eb15fec5b101fbeac62b034
simondriesen <simon.driesen@hotmail.com>
2016-10-11 14:39:06 +0200

    DB data added to controller of dashboard.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -17,6 +17,45 @@ class Controller extends BaseController
     }
 
     public function index(){
-        return view('dashboard');
+        // alle blokken meegeven
+        $blocks = \App\Block::all();
+        $data['blocks'] = $blocks;
+
+        // alle foam types meegeven
+        $foamtypes = \App\FoamType::all();
+        $data['foamtypes'] = $foamtypes;
+
+        // alle prime silos meegeven
+        $primes = \App\PrimeSilo::all();
+        $data['primes'] = $primes;
+
+        // alle waste silos meegeven
+        $wastes = \App\WasteSilo::all();
+        $data['wastes'] = $wastes;
+
+        // alle resources meegeven
+        $recourses = \App\Resource::all();
+        $data['recources'] = $recourses;
+
+        // alle logs meegeven
+        $logs = \App\Log::all();
+        $data['logs'] = $logs;
+
+        return view('dashboard', $data);
     }
 }
+/*
+
+public function create(){
+    $allStudents = \App\Student::all();
+
+    $data['students'] = $allStudents;
+
+    return view('students/create', $data);
+}
+
+@foreach($students as $student)
+        <p>{{ $student->email }}</p>
+@endforeach
+
+*/
