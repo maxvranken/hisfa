cc6bc2f865448f6a09dafac2d35f373d6ea4a236
Ruben Pauwels <rubenp.pauwels@gmail.com>
2016-10-17 23:32:09 +0200

    Changed id as silonumber to $key as silonumber

--- resources/views/focus/primeSilos.blade.php
+++ resources/views/focus/primeSilos.blade.php
@@ -7,9 +7,9 @@
             <a href="/primesilos">Primesilo's</a>
         </div>
         <div class="silo_container">
-            @foreach($primesilos as $primesilo)
-                <div class="silo" id="silo{{ $primesilo->id }}">
-                    <div class="silo_number">{{ $primesilo->id }}</div>
+            @foreach($primesilos as $key=>$primesilo)
+                <div class="silo" id="silo{{ $key+1 }}">
+                    <div class="silo_number">{{ $key+1 }}</div>
                     <div class="silo_number">{{ $primesilo->resource->name}}</div>
                     <div class="silo_fill">
                         <div class="silo_filled" style="height: calc(100% - {{ $primesilo->quantity }}%);"></div>
@@ -31,7 +31,7 @@
                     </a>
                 </div>
 
-
+                
             @endforeach
             <div class="silo" id="silo{{ $primesilo->id }}">
                 <div class="silo_number">Add Silo</div>
